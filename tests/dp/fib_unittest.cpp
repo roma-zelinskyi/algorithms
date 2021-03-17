f7e0f20f860aac9c01a0f1006f2a9801c5ebd0df
Roman Zelinskyi <lord.zelinskyi@gmail.com>
2021-03-17 13:42:56 +0100

    tests: Add fib unit test
    Signed-off-by: Roman Zelinskyi <lord.zelinskyi@gmail.com>

--- tests/dp/fib_unittest.cpp
+++ tests/dp/fib_unittest.cpp
@@ -4,12 +4,18 @@
  *  @author  Roman Zelinskyi <lord.zelinskyi@gmail.com>
  */
 
+#include "dp/fib.hpp"
+
 #include <gtest/gtest.h>
 
 namespace {
 
 TEST(DynamicProgrammingTests, FibTest)
 {
+    ASSERT_EQ(5, zee::fib(5));
+    ASSERT_EQ(13, zee::fib(13));
+    ASSERT_EQ(6765, zee::fib(20));
+    ASSERT_EQ(17711, zee::fib(22));
 }
 
 } // namespace
