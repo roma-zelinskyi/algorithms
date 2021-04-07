bcbaa0697e32876e8f8c14d5e1144cb5ed8fed58
Roman Zelinskyi <lord.zelinskyi@gmail.com>
2021-04-07 23:53:20 +0200

    feat: Add dp fib problem using tabulation
    Signed-off-by: Roman Zelinskyi <lord.zelinskyi@gmail.com>

---
+++ src/dp/tab/fib.hpp
@@ -0,0 +1,16 @@
+/**
+ *  Project Dynamic Programming
+ *
+ *  @author  Roman Zelinskyi <lord.zelinskyi@gmail.com>
+ */
+
+#pragma once
+
+#include <cstdint>
+
+namespace dp::tab {
+
+std::uint32_t fib(std::uint32_t num);
+
+} // namespace dp::tab
+
